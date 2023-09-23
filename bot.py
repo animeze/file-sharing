fb0df72844de104ae87288d8a6afd73844b82422
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-09-23 23:04:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ class Bot(Client):
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
+        await web.TCPSite(app, bind_address, 8).start()
 
     async def stop(self, *args):
         await super().stop()
