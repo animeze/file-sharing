69a41a96a256595e96d3f43530f00f2e8ff44c04
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-09-23 23:07:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,8 +9,10 @@ from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID
 from dotenv import load_dotenv
+
+PORT=8000
 
 load_dotenv(".env")
 
@@ -71,7 +73,7 @@ class Bot(Client):
         #web-response
         app = web.AppRunner(await web_server())
         await app.setup()
-        bind_address = "0.0.0.0"
+        bind_address = "192.0.2.5"
         await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
