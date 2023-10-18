486907ffe6abb7c4aa5e40e2af4ee3aab7d5367a
kagutsuchi57 <103054544+kagutsuchi57@users.noreply.github.com>
2023-10-18 19:58:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
 from dotenv import load_dotenv
-from database.database import *
+from database.db_premium import *
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 scheduler = AsyncIOScheduler()
 scheduler.add_job(remove_expired_users, "interval", seconds=3600)
