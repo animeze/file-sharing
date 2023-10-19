6992f0fd86a11f31a6e38d36c9e6bfd3bb811017
kagutsuchi57 <103054544+kagutsuchi57@users.noreply.github.com>
2023-10-19 19:37:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -241,7 +241,7 @@ async def list_premium_users_command(client, message):
     for user in premium_users:
         user_ids = user["user_id"]
         user_info = await client.get_users(user_ids)
-        username = user_info.username if "@"+user_info.username else user_info.first_name
+        username = user_info.username if user_info.username else user_info.first_name
         expiration_timestamp = user["expiration_timestamp"]
         xt = (expiration_timestamp-(time.time()))
         x = round(xt/(24*60*60))
