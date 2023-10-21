1197cae697d85c466d202ad23d6d370da76aa07b
kagutsuchi57 <103054544+kagutsuchi57@users.noreply.github.com>
2023-10-21 23:09:46 +0530

    Update jav_premium.py

--- plugins/jav_premium.py
+++ plugins/jav_premium.py
@@ -2,11 +2,12 @@ import os
 import asyncio
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
-from helper_func import get_messages
 from bot import Bot
-from config import ADMINS
+from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
+from helper_func import subscribed, encode, decode, get_messages
 from database.db_wes import *
 
 @Bot.on_message(filters.private & filters.command('addjav') & filters.user(ADMINS))
