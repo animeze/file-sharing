f739f51e52799891c0f6ae55db4ffdeb4cb3c482
animeze <130531179+animeze@users.noreply.github.com>
2023-11-04 08:55:29 +0545

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Shankss:Shankss@cluster0.a9ega9w.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "kagut")
+DB_NAME = os.environ.get("DATABASE_NAME", "Shankss")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001199007342"))
