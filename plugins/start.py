43f54772bdf8134988160ffdab7e1c186aac88d7
animeze <130531179+animeze@users.noreply.github.com>
2023-12-24 21:14:56 +0545

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -43,7 +43,7 @@ async def start_command(client: Client, message: Message):
                     await message.reply_text("You're not a premium user. if you want buy premium services then contact @HandsumGuyOp")
                     return
 
-        if string.startswith("am"):
+        if string.startswith("wesporn"):
             if not await is_premium_user(message.from_user.id):
                 if not await wes_premium_user(message.from_user.id):
                     await message.reply_text("You're not a premium user. if you want buy premium services then contact @HandsumGuyOp")
