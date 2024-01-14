69e70d7e3673634fe2a0a0151a2550341121a9ee
animeze <130531179+animeze@users.noreply.github.com>
2024-01-14 15:56:58 +0545

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -47,4 +47,4 @@ async def channel_post(client: Client, message: Message):
     link4 = f"https://telegram.me/{client.username}?start={base64_string}"
     
 
-   await reply_text.edit(f"<b>> Here is your link: </b>{link}\n\n<b>> Premium Desi Link : </b>{link1}\n\n<b>> Premium AV Link : </b>{link2}\n\n<b>> Premium Hentai Link : </b>{link3}\n\n<b>> Premium Onlyfans Link : </b>{link4}", disable_web_page_preview = True)
+   await reply_text.edit(f"<b>> Here is your link: </b>{link}\n\n<b>> Premium Desi Link : </b>{link1}\n\n<b>> Premium Adult Movie Link : </b>{link2}\n\n<b>> Premium Hentai Link : </b>{link3}\n\n<b>> Premium Onlyfans Link : </b>{link4}", disable_web_page_preview = True)
