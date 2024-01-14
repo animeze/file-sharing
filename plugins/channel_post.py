0dded21c54b067d0fc2e4f521b3fe79df75e3ad4
kagutsuchi57 <103054544+kagutsuchi57@users.noreply.github.com>
2024-01-14 17:54:50 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -46,7 +46,18 @@ async def channel_post(client: Client, message: Message):
     base64_string = await encode(string)
     link4 = f"https://telegram.me/{client.username}?start={base64_string}"
     
+    keyboard = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("Free Link", url=link)],
+            [InlineKeyboardButton("Premium Jav Link", url=link1)],
+            [InlineKeyboardButton("Premium Wes Link", url=link2)],
+            [InlineKeyboardButton("Premium Hentai Link", url=link3)],
+            [InlineKeyboardButton("Premium Onlyfans Link", url=link4)]
+        ]
+    )
 
-  keyboard = 
-             InlineKeyboardMarkup( [ [InlineKeyboardButton("Free Link", url=link)], [InlineKeyboardButton("Premium Desi Link", url=link1)], [InlineKeyboardButton("Premium Adult Movie Link", url=link2)], [InlineKeyboardButton("Premium Hentai Link", url=link3)], [InlineKeyboardButton("Premium Onlyfans Link", url=link4)] ] )
-   await reply_text.edit( "<b>> Here are your links:</b>", disable_web_page_preview=True, reply_markup=keyboard )
+    await reply_text.edit(
+        "<b>> Here are your links:</b>",
+        disable_web_page_preview=True,
+        reply_markup=keyboard
+    )
