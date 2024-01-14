0e583a0ba669c9979af4dc90e65e903cd84332d7
animeze <130531179+animeze@users.noreply.github.com>
2024-01-14 15:31:28 +0545

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -57,7 +57,7 @@ async def batch(client: Client, message: Message):
     base64_string = await encode(string)
     link4 = f"https://telegram.me/{client.username}?start={base64_string}"
     
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Link", url=f"{link}")],[InlineKeyboardButton("Jav Link", url=f"{link1}")],[InlineKeyboardButton("Wes Link", url=f"{link2}")],[InlineKeyboardButton("Hentai Link", url=f"{link3}")],[InlineKeyboardButton("OnlyFans Link", url=f"{link4}")]])
+    reply_markup = InlineKeyboardMarkup([InlineKeyboardButton("🆓 Link", url=f"{link}")], [InlineKeyboardButton("Jav Link", url=f"{link1}")], [InlineKeyboardButton("Wes Link", url=f"{link2}")], [InlineKeyboardButton("Hentai Link", url=f"{link3}")], [InlineKeyboardButton("OnlyFans Link", url=f"{link4}")])
     await second_message.reply_text(f"<b>Here are your links.</b>", quote=True, reply_markup=reply_markup)
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
