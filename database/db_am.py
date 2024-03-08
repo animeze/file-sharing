abd4b8bfc08ad597a7b5d7f6ac3ee730bca90fd3
animeze <130531179+animeze@users.noreply.github.com>
2024-03-08 18:31:08 +0545

    Update  db_am.py

--- database/db_am.py
+++ database/db_am.py
@@ -5,30 +5,30 @@ from config import DB_URI, DB_NAME
 
 dbclient = pymongo.MongoClient(DB_URI)
 database = dbclient[DB_NAME]
-wcollection = database['wes-users']
+wcollection = database['am-users']
 
 
-async def wes_add_premium(user_id, time_limit_days):
+async def am_add_premium(user_id, time_limit_days):
     expiration_timestamp = int(time.time()) + time_limit_days * 24 * 60 * 60
     premium_data = {
         "user_id": user_id,
         "expiration_timestamp": expiration_timestamp,
     }
-    wcollection.insert_one(premium_data)
+    acollection.insert_one(premium_data)
 
-async def wes_remove_premium(user_id):
-    result = wcollection.delete_one({"user_id": user_id})
+async def am_remove_premium(user_id):
+    result = acollection.delete_one({"user_id": user_id})
 
-async def wes_remove_expired_users():
+async def am_remove_expired_users():
     current_timestamp = int(time.time())
 
     # Find and delete expired users
-    expired_users = wcollection.find({"expiration_timestamp": {"$lte": current_timestamp}})
+    expired_users = acollection.find({"expiration_timestamp": {"$lte": current_timestamp}})
     
     for expired_user in expired_users:
         user_id = expired_user["user_id"]
-        wcollection.delete_one({"user_id": user_id})
+        acollection.delete_one({"user_id": user_id})
 
-async def wes_premium_user(user_id):
-    user = wcollection.find_one({"user_id": user_id})
+async def am_premium_user(user_id):
+    user = acollection.find_one({"user_id": user_id})
     return user is not None
